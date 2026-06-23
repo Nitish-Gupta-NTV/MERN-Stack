f98ef66034023d6ce2de22ffd5ad1869ade35b66
Nitish- Gupta-NTV <183982429+Nitish-Gupta-NTV@users.noreply.github.com>
2026-06-23 15:04:09 +0530

    html and css

---
+++ day3/script.js
@@ -0,0 +1,34 @@
+let input=parseInt(prompt("enter a message"));
+switch(input)
+{
+    case 1:
+        "Monday"
+        console.log("monday")
+        break;
+        case 2:
+        "Tuesday"
+        console.log("Tuesday")
+        break;
+        case 3:
+        "wednesday"
+        console.log("wednesday")
+        break;
+        case 4:
+        "thuraday"
+        console.log("thrusday")
+        break;
+        case 5:
+        "friday"
+        console.log("friday")
+        break;
+        case 6:
+        "Monday"
+        console.log("saturday")
+        break;
+        case 7:
+        "sunday"
+        console.log("sunday")
+        break;
+        default:
+            console.log("invalid input");
+}
---
+++ day3/script1.js
@@ -0,0 +1,23 @@
+let h1=document.querySelector("h1");
+console.log(h1);
+h1.innerHTML="ghvhv"
+/*let input =parseInt(prompt("enter yours marks"));
+if(input>11&&input<20)
+{
+    console.log("D");
+}
+else if(input=>21&&input<=30)
+{
+    console.log("c");
+}
+else if(input>31&&input<40)
+{
+    console.log("B");
+}
+else if(input>41&&input<50)
+{
+    console.log("A");
+}
+else{
+    console.log("E")
+}*/
